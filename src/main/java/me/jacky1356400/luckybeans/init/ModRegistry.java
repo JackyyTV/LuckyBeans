cd5d26f0c47118136f5b6fd354afde3f620f91d2
Jacky1356400 <jacky1356400@hotmail.com>
2017-08-04 07:36:49 +0800

    Just a bit of minor changes
    - Updated Forge, mappings and JEI
    - Removed RecipeHelper and related stuff as I don't need to use such a
    big helper class for just a single recipe

--- src/main/java/me/jacky1356400/luckybeans/init/ModRegistry.java
+++ src/main/java/me/jacky1356400/luckybeans/init/ModRegistry.java
@@ -3,7 +3,6 @@ package me.jacky1356400.luckybeans.init;
 import me.jacky1356400.luckybeans.block.BlockBeanLeaves;
 import me.jacky1356400.luckybeans.block.BlockBeanSapling;
 import me.jacky1356400.luckybeans.block.BlockMysteriousBean;
-import me.jacky1356400.luckybeans.helper.RecipeHelper;
 import me.jacky1356400.luckybeans.item.ItemBean;
 import me.jacky1356400.luckybeans.item.ItemCreativeTreeGen;
 import me.jacky1356400.luckybeans.item.ItemMysteriousBean;
@@ -11,38 +10,44 @@ import me.jacky1356400.luckybeans.util.Data;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import static me.jacky1356400.luckybeans.util.Data.MODID;
 
 public class ModRegistry {
 
-	public static final Block BEANLEAVES = new BlockBeanLeaves();
-	public static final Block BEANSAP = new BlockBeanSapling();
-	public static final Block MYSTBEANBLOCK = new BlockMysteriousBean();
+    public static final Block BEANLEAVES = new BlockBeanLeaves();
+    public static final Block BEANSAP = new BlockBeanSapling();
+    public static final Block MYSTBEANBLOCK = new BlockMysteriousBean();
 
-	public static final Item MYSTBEAN = new ItemMysteriousBean();
-	public static final Item BEAN = new ItemBean();
+    public static final Item MYSTBEAN = new ItemMysteriousBean();
+    public static final Item BEAN = new ItemBean();
     public static final Item TREEGEN = new ItemCreativeTreeGen();
 
-	private static void initRecipes() {
-		RecipeHelper.addShapeless(BEANSAP, ModRegistry.MYSTBEAN, Blocks.SAPLING);
-	}
+    @SubscribeEvent
+    public void onBlockRegistry(RegistryEvent.Register<Block> e) {
+        e.getRegistry().registerAll(Data.BLOCKS.toArray(new Block[0]));
+    }
 
-	@SubscribeEvent
-	public void onBlockRegistry(RegistryEvent.Register<Block> e) {
-		e.getRegistry().registerAll(Data.BLOCKS.toArray(new Block[0]));
-	}
+    @SubscribeEvent
+    public void onItemRegistry(RegistryEvent.Register<Item> e) {
+        e.getRegistry().registerAll(Data.ITEMS.toArray(new Item[0]));
+    }
 
-	@SubscribeEvent
-	public void onItemRegistry(RegistryEvent.Register<Item> e) {
-		e.getRegistry().registerAll(Data.ITEMS.toArray(new Item[0]));
-	}
-
-	@SubscribeEvent
-	public void onRecipeRegistry(RegistryEvent.Register<IRecipe> e) {
-		initRecipes();
-		e.getRegistry().registerAll(Data.RECIPES.toArray(new IRecipe[0]));
-	}
+    @SubscribeEvent
+    public void onRecipeRegistry(RegistryEvent.Register<IRecipe> e) {
+        GameRegistry.addShapelessRecipe(
+                new ResourceLocation(MODID, "bean_sapling"),
+                new ResourceLocation(MODID, "bean_sapling"), new ItemStack(BEANSAP, 1),
+                Ingredient.fromStacks(new ItemStack(Blocks.SAPLING, 1)),
+                Ingredient.fromStacks(new ItemStack(MYSTBEAN, 1))
+        );
+    }
 
 }
--- src/main/java/me/jacky1356400/luckybeans/util/Data.java
+++ src/main/java/me/jacky1356400/luckybeans/util/Data.java
@@ -5,7 +5,6 @@ import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +13,6 @@ public class Data {
 
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	public static final List<IRecipe> RECIPES = new ArrayList<IRecipe>();
 	public static final String VERSION = "1.0";
 	public static final String MODID = "luckybeans";
 	public static final String MODNAME = "LuckyBeans";
